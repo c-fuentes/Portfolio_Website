cf1448503a6fabb9e1172b1fcda598cfff4256d2
c-fuentes <celsofuentes957@gmail.com>
2025-10-02 16:45:48 -0400

    added content to data analysis page, needs formatting

---
+++ portfolio/app/DA/page.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-start ">
+        <p>Investigation into historical bias from referees in the NBA</p>
+        <div>
+          <p>
+            This was my first project in data analysis. I had some previous expereince with SQL by using it for database design and implementation. Therefore, our two person team (me and <a src="
+            https://www.linkedin.com/in/levi-riggins-154bbb289/">Levi Riggins</a>) wanted to use as many new tools to try and answer our question: Do referees in 
+            the NBA really have a bias towards teams or for the home team/away team.
+            We found the dataset on <a src="https://www.kaggle.com/datasets/wyattowalsh/basketball">Kaggle</a>, decided to try SQLite as our db, use python pandas to data clean and wanted to use Tableau for our visualizations.
+            However during the data transformation process (in SQLite), I incorrectly calculated z-scores and also had not filtered out referees that had very few games during data cleaning. 
+            This resulted in very skewed data results that were then put into Tableau. We believed that Tableau was reading our results wrong so we ended up switching to PowerBI. 
+            It wasn't until later that we realized my mistake but by then we just decided to stick with PowerBI. So in the end we used the original dataset from Kaggle, data cleaned with python, did small transformations in SQLite, did most transformations and calculation with DAX in Microsoft PowerBI, and created our report in Microsoft PowerBI.
+            Ultimately we found that no referees exhibted enough of a deviation from the average to be an outlier (aka biased without a doubt) but there were some very close calls.
+            The report was fully interactive letting users select specific teams or specific referees and allowed for navigation.
+            PowerBI does not allow me to embed the report with my personal Microsoft account so I am going to show some screen captures of the referees who were close to being outliers. 
+          </p>
+          <div>
+             <Image src="/images/Capture.png" alt="" width="1000" height="800"/> 
+          </div>
+
+          <div>
+             <Image src="/images/Overall.png" alt="" width="1000" height="800"/> 
+          </div>
+          
+          <div>
+             <Image src="/images/WolvesHome.png" alt="" width="1000" height="800"/> 
+          </div>
+
+          <div>
+             <Image src="/images/Away.png" alt="" width="1000" height="800"/> 
+          </div>
+        </div>
+
+      </main>
+    </div>
+  );
+}
