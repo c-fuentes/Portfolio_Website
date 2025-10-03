3c923c61047f35f58df35696088b0d4ed1652ef4
c-fuentes <97372834+c-fuentes@users.noreply.github.com>
2025-10-02 21:06:23 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/Portfolio_Website",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
