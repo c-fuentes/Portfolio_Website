fee9f2b344fddfc76801fd268db0c54a2cad5ad6
c-fuentes <celsofuentes957@gmail.com>
2025-10-09 11:57:12 -0400

    created the sites for the other sections

---
+++ app/EDA/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-start ">
+        <div>
+            <ul>
+                <h1>Table of Contents</h1>
+                <li>
+                    Exploratory Data Analysis on a PnL report
+                </li>
+            </ul>
+        </div>
+        <div>
+            
+        </div>
+      </main>
+    </div>
+  );
+}
