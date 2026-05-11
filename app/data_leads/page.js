18cd736af9b3f9670c3ae1b547203137374ea930
c-fuentes <celsofuentes957@gmail.com>
2026-05-10 21:59:50 -0400

    trying button changes and added description for checkboxes

--- app/data_leads/page.js
+++ app/data_leads/page.js
@@ -8,14 +8,15 @@ export default function Home() {
             <p>placeholder actions for later, need to figure out how to make the post and get work for next js and what server to hookup to, and aws stuff</p>
             <form action="/action_page.php" method="post">
                 <label for="fname">First name:</label><br></br>
-                <input className="bg-white outline-black" type="text" id="fname" name="fname" value="John" pattern="[A-Za-z]{20}" required autocomplete="on"></input><br></br>
+                <input className="bg-white outline-black" type="text" id="fname" name="fname" placeholder="John" pattern="[A-Za-z]{20}" required autocomplete="on"></input><br></br>
                 <label for="lname">Last name:</label><br></br>
-                <input className="bg-white outline-black" type="text" id="lname" name="lname" value="Doe"  pattern="[A-Za-z]{20}" required autocomplete="on"></input><br></br>
+                <input className="bg-white outline-black" type="text" id="lname" name="lname" placeholder="Doe"  pattern="[A-Za-z]{20}" required autocomplete="on"></input><br></br>
                 <label for="pnum">Phone Number:</label><br></br>
                 <input className="bg-white outline-black" type="tel" id="pnum" name="pnum" placeholder="404-999-9999" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required autocomplete="on"></input><br></br>
                 <label for="email">Enter your email:</label><br></br>
                 <input className="bg-white outline-black" type="email" id="email" name="email" autocomplete="on"></input><br></br>
                 
+                <p>What kind(s) of insurance are you looking to get quoted for?</p>
                 <input type="checkbox" id="auto" name="auto" value="Auto Insurance"></input>
                 <label for="auto">Auto Insurance</label><br></br>
                 <input type="checkbox" id="home" name="home" value="Home"></input>
@@ -27,7 +28,7 @@ export default function Home() {
                 <input type="checkbox" id="other" name="other" value="other"></input>
                 <label for="other">Small Business</label><br></br>
 
-                <input className="bg-blue" type="submit" value="Submit" ></input>
+                <input className="bg-red" type="submit" value="Submit" ></input>
             </form>
 
             <p>By filling out the form, you are providing express consent by electronic signature that you may be contacted by telephone .via call and/or text messages. and/or email for marketing purposes</p>
